f2bd9aee52386ab0755b3c9a2360ee86683b8f85
Danny Connolly <dannyconnolly@email.com>
2014-05-05 22:44:55 +0100

    changed laravel event alias in app.php

--- app/views/events/index.blade.php
+++ app/views/events/index.blade.php
@@ -1,76 +1,51 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>H Manager</title>
-        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-    </head>
-    <body>
-        <div class="container">
+<h1>All the Events</h1>
 
-            <nav class="navbar navbar-inverse">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="{{ URL::to('events') }}">Events Alert</a>
-                </div>
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ URL::to('users') }}">View All Users</a></li>
-                    <li><a href="{{ URL::to('users/create') }}">Create a User</a></li>
-                    <li><a href="{{ URL::to('events') }}">View All Events</a></li>
-                    <li><a href="{{ URL::to('events/create') }}">Create a Event</a></li>
-                </ul>
-            </nav>
+<!-- will be used to show any messages -->
+@if (Session::has('message'))
+<div class="alert alert-info">{{ Session::get('message') }}</div>
+@endif
 
-            <h1>All the Events</h1>
+<table class="table table-striped table-bordered">
+    <thead>
+        <tr>
+            <td>ID</td>
+            <td>Title</td>
+            <td>Type</td>
+            <td>Hostel</td>
+            <td>County</td>
+            <td>Date From</td>
+            <td>Date To</td>
+            <td>Actions</td>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($events as $key => $value)
+        <tr>
+            <td>{{ $value->id }}</td>
+            <td>{{ $value->title }}</td>
+            <td>{{ $value->type }}</td>
+            <td>{{ $value->hostel }}</td>
+            <td>{{ $value->county }}</td>
+            <td>{{ $value->date_from }}</td>
+            <td>{{ $value->date_to }}</td>
 
-            <!-- will be used to show any messages -->
-            @if (Session::has('message'))
-            <div class="alert alert-info">{{ Session::get('message') }}</div>
-            @endif
+            <!-- we will also add show, edit, and delete buttons -->
+            <td>
 
-            <table class="table table-striped table-bordered">
-                <thead>
-                    <tr>
-                        <td>ID</td>
-                        <td>Title</td>
-                        <td>Type</td>
-                        <td>Hostel</td>
-                        <td>County</td>
-                        <td>Date From</td>
-                        <td>Date To</td>
-                        <td>Actions</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($events as $key => $value)
-                    <tr>
-                        <td>{{ $value->id }}</td>
-                        <td>{{ $value->title }}</td>
-                        <td>{{ $value->type }}</td>
-                        <td>{{ $value->hostel }}</td>
-                        <td>{{ $value->county }}</td>
-                        <td>{{ $value->date_from }}</td>
-                        <td>{{ $value->date_to }}</td>
+                <!-- delete the event (uses the destroy method DESTROY /events/{id} -->
+                <!-- we will add this later since its a little more complicated than the other two buttons -->
+                {{ Form::open(array('url' => 'events/' . $value->id, 'class' => 'pull-right')) }}
+                {{ Form::hidden('_method', 'DELETE') }}
+                {{ Form::submit('Delete this Event', array('class' => 'btn btn-warning')) }}
+                {{ Form::close() }}
+                <!-- show the event (uses the show method found at GET /events/{id} -->
+                <a class="btn btn-small btn-success" href="{{ URL::to('events/' . $value->id) }}">Show this Event</a>
 
-                        <!-- we will also add show, edit, and delete buttons -->
-                        <td>
+                <!-- edit this event (uses the edit method found at GET /events/{id}/edit -->
+                <a class="btn btn-small btn-info" href="{{ URL::to('events/' . $value->id . '/edit') }}">Edit this Event</a>
 
-                            <!-- delete the event (uses the destroy method DESTROY /events/{id} -->
-                            <!-- we will add this later since its a little more complicated than the other two buttons -->
-                            {{ Form::open(array('url' => 'events/' . $value->id, 'class' => 'pull-right')) }}
-                            {{ Form::hidden('_method', 'DELETE') }}
-                            {{ Form::submit('Delete this Event', array('class' => 'btn btn-warning')) }}
-                            {{ Form::close() }}
-                            <!-- show the event (uses the show method found at GET /events/{id} -->
-                            <a class="btn btn-small btn-success" href="{{ URL::to('events/' . $value->id) }}">Show this Event</a>
-
-                            <!-- edit this event (uses the edit method found at GET /events/{id}/edit -->
-                            <a class="btn btn-small btn-info" href="{{ URL::to('events/' . $value->id . '/edit') }}">Edit this Event</a>
-
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-
-        </div>
-    </body>
-</html>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
--- app/views/events/show.blade.php
+++ app/views/events/show.blade.php
@@ -1,38 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>H Manager</title>
-        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-    </head>
-    <body>
-        <div class="container">
+<h1>Showing {{ $event->title }}</h1>
 
-            <nav class="navbar navbar-inverse">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="{{ URL::to('events') }}">Event Alert</a>
-                </div>
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ URL::to('users') }}">View All Users</a></li>
-                    <li><a href="{{ URL::to('users/create') }}">Create a User</a></li>
-                    <li><a href="{{ URL::to('events') }}">View All Events</a></li>
-                    <li><a href="{{ URL::to('events/create') }}">Create a Event</a></li>
-                </ul>
-            </nav>
-
-            <h1>Showing {{ $event->title }}</h1>
-
-            <div class="jumbotron text-center">
-                <h2>{{ $event->title }}</h2>
-                <p>
-                    <strong>Type:</strong> {{ $event->type }}<br>
-                    <strong>Hostel:</strong> {{ $event->hostel }}<br>
-                    <strong>County:</strong> {{ $event->county }}<br>
-                    <strong>Date From:</strong> {{ $event->date_from }}<br>
-                    <strong>Date To:</strong> {{ $event->date_to }}<br>
-                    <strong>Details:</strong> {{ $event->details }}
-                </p>
-            </div>
-
-        </div>
-    </body>
-</html>
+<div class="jumbotron text-center">
+    <h2>{{ $event->title }}</h2>
+    <p>
+        <strong>Type:</strong> {{ $event->type }}<br>
+        <strong>Hostel:</strong> {{ $event->hostel }}<br>
+        <strong>County:</strong> {{ $event->county }}<br>
+        <strong>Date From:</strong> {{ $event->date_from }}<br>
+        <strong>Date To:</strong> {{ $event->date_to }}<br>
+        <strong>Details:</strong> {{ $event->details }}
+    </p>
+</div>
