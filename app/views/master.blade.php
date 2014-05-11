ffc38899e9d90c378147904646f9dd036f66b57b
Danny Connolly <dannyconnolly@email.com>
2014-05-11 09:41:39 +0100

    changes to master.blade

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -24,7 +24,7 @@
                 padding-top: 60px;
                 height: 100%;
             }
-            .table>thead>tr>th{
+            .table>thead>tr>th, .table>tbody>tr>td{ 
                 vertical-align: middle;
             }
             .container{
@@ -36,52 +36,66 @@
             .container> .row{
                 margin: 0;
             }
+            footer{
+                text-align: center;
+                padding: 40px 0;
+            }
         </style>
     </head>
     <body>
 
 
-        <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="{{ URL::to('/') }}">H Manager</a>
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="{{ URL::to('/') }}">H Manager</a>
+                </div>
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        @if(Auth::check())
+                        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Users<b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="{{ URL::to('users') }}">Users</a></li>
+                                <li><a href="{{ URL::to('users/create') }}">Create a User</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Members<b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="{{ URL::to('members') }}">Members</a></li>
+                                <li><a href="{{ URL::to('members/create') }}">Create a Member</a></li>
+                                <li class="divider"></li>
+                                <li><a href="{{ URL::to('membertypes') }}">Member Types</a></li>
+                                <li><a href="{{ URL::to('membertypes/create') }}">Create a Member Type</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Hostels<b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="{{ URL::to('hostels') }}">Hostels</a></li>
+                                <li><a href="{{ URL::to('hostels/create') }}">Create a Hostel</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Events<b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="{{ URL::to('events') }}">Events</a></li>
+                                <li><a href="{{ URL::to('events/create') }}">Create an Event</a></li>
+                                <li class="divider"></li>
+                                <li><a href="{{ URL::to('eventtypes') }}">Event Types</a></li>
+                                <li><a href="{{ URL::to('eventtypes/create') }}">Create an Event Type</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="{{ URL::to('logout') }}">Logout</a></li>
+                        @else 
+                        <li><a href="{{ URL::to('login') }}">Login</a></li>
+                        @endif
+                    </ul>
+                </div>
             </div>
-            <ul class="nav navbar-nav">
-                @if(Auth::check())
-                <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Users<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="{{ URL::to('users') }}">Users</a></li>
-                        <li><a href="{{ URL::to('users/create') }}">Create a User</a></li>
-                    </ul>
-                </li>
-                <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Members<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="{{ URL::to('members') }}">Members</a></li>
-                        <li><a href="{{ URL::to('members/create') }}">Create a Member</a></li>
-                        <li class="divider"></li>
-                        <li><a href="{{ URL::to('membertypes') }}">Member Types</a></li>
-                        <li><a href="{{ URL::to('membertypes/create') }}">Create a Member Type</a></li>
-                    </ul>
-                </li>
-                <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Hostels<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="{{ URL::to('hostels') }}">Hostels</a></li>
-                        <li><a href="{{ URL::to('hostels/create') }}">Create a Hostel</a></li>
-                    </ul>
-                </li>
-                <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Events<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="{{ URL::to('events') }}">Events</a></li>
-                        <li><a href="{{ URL::to('events/create') }}">Create an Event</a></li>
-                        <li class="divider"></li>
-                        <li><a href="{{ URL::to('eventtypes') }}">Event Types</a></li>
-                        <li><a href="{{ URL::to('eventtypes/create') }}">Create an Event Type</a></li>
-                    </ul>
-                </li>
-                <li><a href="{{ URL::to('logout') }}">Logout</a></li>
-                @else 
-                <li><a href="{{ URL::to('login') }}">Login</a></li>
-                @endif
-            </ul>
         </nav>
         <div class="container">
             <div class="row">
